45d2957a44cc737063999d8edaab90c4d2578586
Taras Nechyporuck <tarasnechyporuck@gmail.com>
2023-12-30 20:51:12 +0200

    make build

---
+++ index.404bdf70.js
@@ -0,0 +1,3 @@
+var e;function s(s){var t,l=document.getElementsByClassName("header__cover--right--background");for(s>l.length&&(e=1),s<1&&(e=l.length),t=0;t<l.length;t++)l[t].style.display="none",l[t].classList.remove("slide-transition");l[e-1].style.display="block",l[e-1].classList.add("slide-transition")}//# sourceMappingURL=index.404bdf70.js.map
+s(e=1),s(e);
+//# sourceMappingURL=index.404bdf70.js.map
